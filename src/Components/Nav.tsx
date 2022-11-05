012d18b03232e2affa1dd624ff3dccb6c3cd790c
WinterSunset95 <wintersunset95@gmail.com>
2022-11-05 10:45:41 +0530

    small animations on the header

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -5,17 +5,25 @@
 import {useState, useEffect } from 'react'
 
 const Nav = () => {
-	window.addEventListener('scroll', () => console.log(window.scrollY))
+	const [top, setTop] = useState(true)
+	const [full, setFull] = useState(false)
+	window.addEventListener('scroll', () => window.scrollY > 40 ? setTop(false) : setTop(true))
+	useEffect(() => {
+		console.log(top)
+	}, [top])
 	return (
-		<nav className="w-screen Montserrat text-md flex justify-between items-center p-2 md:p-4 text-[#f6f6f6] font-[300] bg-[rgba(20,20,20,0.3)] sticky top-0">
+		<nav className={`${top ? 'w-screen md:w-9/12' : 'w-screen sticky top-0 bg-[rgba(20,20,20,0.3)]'} duration-300 Montserrat text-md flex justify-between items-center p-2 md:p-4 text-gray-100 font-[300] text-base md:text-xl`}>
 			WinterSunset95
-			<ul className="flex justify-around items-center text-sm">
-				<li className="mr-2">Home</li>
-				<li className="mr-2">About</li>
-				<li className="mr-2">Projects</li>
-				<li className="mr-2">TIL</li>
-				<li className="mr-2">Contact</li>
+			<ul className={`${full ? 'w-0' : 'w-auto'} duration-500 flex justify-around items-center text-sm md:text-lg overflow-hidden md:w-auto `}>
+				<li className={`mr-2 md:mr-4 cursor-pointer hover:font-[500]`}>Home</li>
+				<li className={`mr-2 md:mr-4 cursor-pointer hover:font-[500]`}>About</li>
+				<li className={`mr-2 md:mr-4 cursor-pointer hover:font-[500]`}>Projects</li>
+				<li className={`mr-2 md:mr-4 cursor-pointer hover:font-[500]`}>TIL</li>
+				<li className={`mr-2 md:mr-4 cursor-pointer hover:font-[500]`}>Contact</li>
 			</ul>
+			<button className="w-4 h-4 md:hidden" onClick={() => setFull(!full)}>
+				<svg className="w-full h-full" fill="gray" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+			</button>
 		</nav>
 	)
 }
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -6,11 +6,12 @@
 import {useState, useEffect} from 'react'
 import Nav from '../Components/Nav'
 import horror from '../assets/backgrounds/horror.jpg'
+import street_aesthetic from '../assets/backgrounds/street_aesthetic.jpg'
 
 const Home = () => {
 	return (
 		<div className="flex flex-col justify-start items-center w-full relative h-screen">
-			<img src={horror} className="absolute w-full top-0 -z-10" alt="Horror bg"/>
+			<img src={street_aesthetic} className="absolute w-full top-0 -z-10" alt="Horror bg"/>
 			<Nav />
 			This is the home page
 		</div>
