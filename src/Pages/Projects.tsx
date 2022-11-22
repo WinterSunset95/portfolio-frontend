740ff3fb241ba11dcb370758c2491ea4605c217f
WinterSunset95 <wintersunset95@gmail.com>
2022-11-22 20:30:13 +0530

    started with the projects page

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -4,10 +4,22 @@
 
 
 import { useState, useEffect } from 'react'
+import data from '../assets/data/products.json'
 
 const Projects = () => {
 	return (
-		<div>Hi humans</div>
+		<div className="mx-20 backdrop-blur-sm">
+			{
+				data.map(item => {
+					return (
+						<div className={`drop-shadow-md`}>
+							{item.name}
+							<img src={item.image} alt="" />
+						</div>
+					)
+				})
+			}
+		</div>
 	)
 }
 
