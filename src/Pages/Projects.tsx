975b6ad08dd6695dee77d743363120bc1c0efd12
WinterSunset95 <wintersunset95@gmail.com>
2022-11-23 22:18:29 +0530

    started with the projects page

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -6,15 +6,21 @@
 import { useState, useEffect } from 'react'
 import data from '../assets/data/products.json'
 
+// tailwind drop shadow
+// 
+
 const Projects = () => {
 	return (
-		<div className="mx-20 backdrop-blur-sm">
+		<div className="px-20 backdrop-blur-sm flex wrap">
 			{
 				data.map(item => {
 					return (
-						<div className={`drop-shadow-md`}>
-							{item.name}
-							<img src={item.image} alt="" />
+						<div key={item.name} className={`drop-shadow-md m-4 relative w-fit h-fit`}>
+							<div className={`w-full h-full absolute bottom-0 left-0 flex flex-col justify-end items-end bg-gradient-to-br from-[rgba(0,0,0,0)] via-[rgba(0,0,0,0.8)] to-[rgba(0,0,0,1)] p-4`}>
+								<span className={`text-end MontAlt text-white`}>{item.name}</span>
+								<span className={`text-end MontAlt text-white`}>{item.description}</span>
+							</div>
+							<img src={item.image} alt="" className={``}/>
 						</div>
 					)
 				})
